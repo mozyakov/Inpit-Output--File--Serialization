15ca8ed287b0472d5abfc90e41a645336f790428
mozyakov <s.mozyakov@gmail.com>
2023-03-28 21:14:18 +0700

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+        File dir1 = new File("C://SomeDir");  // создаем объект File в качестве каталога
+    }
+}
