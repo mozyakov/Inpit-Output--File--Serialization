ee482f04a9a0fb22a551da10f7838e268b48dcbe
mozyakov <s.mozyakov@gmail.com>
2023-03-28 23:16:16 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,16 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
+        //File dir1 = new File("C://SomeDir");  // создаем объект File в качестве каталога
+        //File file1 = new File("C://SomeDir", "Hello.txt"); // создаем объект File для файла, находящегося в каталоге
+        //File file2 = new File(dir1, "Hello2.txt"); // создаем объект File для файла, находящегося в каталоге dir1
 
-
-
-
-        File dir1 = new File("C://SomeDir");  // создаем объект File в качестве каталога
+        File dir = new File("Somedir"); // определяем объект для каталога
+        if(dir.mkdir())    // создать каталог
+            System.out.println("Каталог создан");
+        File newDir = new File("newDir)"); // определяем новый объект для каталога
+        dir.renameTo(newDir);   // переименовать каталог
+        if(newDir.delete())
+           System.out.println("Каталог удален");
     }
 }
